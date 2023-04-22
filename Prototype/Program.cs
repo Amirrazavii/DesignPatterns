0e9d8103eac1b188f4246eb891601a12a4b24ccb
AmirRazavi <razavi77amir@gmail.com>
2023-04-23 01:57:38 +0330

    Prototyoe

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Prototype;
+
+interface IPrototype
+{
+    Book Clone();
+}
+public class Book :IPrototype{
+
+    private string title;
+    private int price;
+
+   public Book(string title, int price)
+    {
+        this.title=title;
+        this.price=price;
+
+    }
+
+    
+
+    public Book Clone()
+    {
+        return new Book(this.title, this.price);
+    }
+}
+
+
+
+
+public static class program
+{
+    static void Main(string[] args)
+    {
+        var orginaled = new Book("C#",20);
+        var copy =orginaled.Clone();
+
+        
+
+        Console.ReadLine();
+
+    }
+}
