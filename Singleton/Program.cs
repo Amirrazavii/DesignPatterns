c74b5f311a29e467f51aebd3769e2ecad420fa27
AmirRazavi <razavi77amir@gmail.com>
2023-04-23 18:17:16 +0330

    SingleTon

---
+++ Singleton/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Singleton;
+
+
+public class Singleton
+{
+
+    private static Singleton instance;
+
+    private Singleton() { }
+
+    public static Singleton GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        Singleton a1 = Singleton.GetInstance();
+
+      
+
+        Console.ReadLine();
+
+    }
+}
